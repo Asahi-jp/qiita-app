814324c6994800ad0cdc2993ec7abfb3ce13637b
Asahi-jp <miuraasahi1@gmail.com>
2022-04-08 18:27:40 +0900

    検索機能を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,12 @@
 class HomeController < ApplicationController
   def index
+    @q = Item.ransack(params[:q])
     @page_num = params[:page]
-    @items = Item.all.includes(:tags, :user).order(likes_count: :desc).page(@page_num).per(10)
+    @items = @q.result.order(likes_count: :desc).includes(:tags, :user).page(@page_num).per(10)
 
+    
+    
+    
     respond_to do |format|
       format.html
       format.js
