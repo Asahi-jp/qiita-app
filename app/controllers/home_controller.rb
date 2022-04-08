d6b50c5d0ca72a60dd10f45c339d89b6880d6c3b
Asahi-jp <miuraasahi1@gmail.com>
2022-04-08 17:37:38 +0900

    kaminariの導入

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @items = Item.all.includes(:tags, :user).order(likes_count: :desc)
+    @items = Item.all.includes(:tags, :user).order(likes_count: :desc).page(params[:page]).per(10)
   end
 end
