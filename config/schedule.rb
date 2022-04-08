03d857123d42e065ab0ab1fbbc8d4556e5793102
Asahi-jp <miuraasahi1@gmail.com>
2022-04-08 17:12:09 +0900

    cronの設定

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,6 +21,8 @@
 
 # Rails.rootを使用するために必要
 require File.expand_path(File.dirname(__FILE__) + "/environment")
+# .zshrcとrbenvのパスを指定するrakeを定義
+job_type :rake, "source /Users/docha/.zshrc; export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 # cronを実行する環境変数
 rails_env = ENV['RAILS_ENV'] || :development
 # cronを実行する環境変数をセット
@@ -28,7 +30,12 @@ set :environment, rails_env
 # cronのログの吐き出し場所
 set :output, "#{Rails.root}/log/cron.log"
 
-# 毎日午前9時に実行
-every 1.day, at: '9:00 am' do
-  runner "Batch::Qiita.run"
+# 一時間毎に実行
+every :hour do
+  rake 'article_state_check:article_state'
 end
+
+# 毎日午前9時に実行
+# every 1.day, at: '9:00 am' do
+#   runner "Batch::Qiita.run"
+# end
