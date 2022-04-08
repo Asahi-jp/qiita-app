bb14a3bffb1c53650bd341cc228c830fa5da1d04
Asahi-jp <miuraasahi1@gmail.com>
2022-04-08 17:43:50 +0900

    ボタンで読み込む処理を実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @items = Item.all.includes(:tags, :user).order(likes_count: :desc).page(params[:page]).per(10)
+    @page_num = params[:page]
+    @items = Item.all.includes(:tags, :user).order(likes_count: :desc).page(@page_num).per(10)
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
